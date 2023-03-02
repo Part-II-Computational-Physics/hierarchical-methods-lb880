Fixed bug in acceleration calculation, still somewhat unstable

classes.py
@@ -32,13 +32,11 @@ class Universe():
             for j,x2 in enumerate(self.body_x[i+1:], i+1):
                 # r points object 1 to object 2
                 r = x1 - x2
-                mag_r = np.abs(r)
-                norm_r = r / mag_r
+                mag_r = np.linalg.norm(r)
+                dir_r = r / mag_r
                 mag_F = self.G / (mag_r**2) # can later multiply in masses
                 # force felt by 1 points at 2
-                F = - mag_F * norm_r
+                F = - mag_F * dir_r
                 self.body_a[i] += F * self.body_m[j]
                 self.body_a[j] -= F * self.body_m[i]
-
-        print(self.body_a)
     

main.py
@@ -10,19 +10,25 @@ matplotlib.use('TkAgg')
 from classes import Universe
 
 if __name__ == '__main__':
-    num_bodies = 2
+    num_bodies = 5
     size = 2
-    G = 0.1
-    dt = 0.01
+    G = 8
+    dt = 0.001
 
     universe = Universe(num_bodies, size, dt, G)
 
+    r = 1
+    v = np.sqrt(G * (num_bodies-1) / r)
     for i in range(num_bodies):
-        x = random.uniform(-size/2, size/2)
-        y = random.uniform(-size/2, size/2)
-        universe.body_x[i] = np.array([x,y])
-        theta = np.arctan2(y,x)
-        universe.body_v[i] = 1e-2 * random.uniform(0.3,1) * np.array([-np.sin(theta), np.cos(theta)])
+        theta = (i/num_bodies) * 2 * np.pi
+        universe.body_x[i] = r * np.array([np.cos(theta),np.sin(theta)])
+        universe.body_v[i] = v * np.array([-np.sin(theta),np.cos(theta)])
+
+    # for i in range(num_bodies):
+    #     r = random.uniform(0,1)
+    #     theta = random.uniform(0,2*np.pi)
+    #     universe.body_x[i] = r * np.array([np.cos(theta),np.sin(theta)])
+    #     universe.body_v[i] = 1.7 * np.sqrt(num_bodies * G * r) * np.array([-np.sin(theta),np.cos(theta)])
 
     fig, ax = plt.subplots()
     ax.set_xlim(-size, size)