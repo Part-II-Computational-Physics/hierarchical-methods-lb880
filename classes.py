8a6a071ff5240ef8e68723e9d83064bc7a0cb1ba
Liam Brown <lffbrown@hotmail.co.uk>
2023-03-02 15:08:56 +0000

    First (bad) gravity implementation

--- classes.py
+++ classes.py
@@ -10,29 +10,35 @@ class Universe():
                  dt = 1,
                  G = 1,
                 ) -> None:
+        self.num_bodies = num_bodies
         self.size = size
         self.dt = dt
         self.G = G
         self.body_x = np.zeros((num_bodies, 2))
         self.body_v = np.zeros((num_bodies, 2))
+        self.body_a = np.zeros((num_bodies, 2))
+        self.body_m = np.ones(num_bodies)
 
     def update_positions(self):
+        self.calculate_accelerations()
+
+        self.body_v += self.dt * self.body_a
         self.body_x += self.dt * self.body_v
 
+    def calculate_accelerations(self):
+        self.body_a = np.zeros((self.num_bodies, 2))
 
-class Body():
-    """Body class for all objects
-    """
+        for i,x1 in enumerate(self.body_x[:-1]):
+            for j,x2 in enumerate(self.body_x[i+1:], i+1):
+                # r points object 1 to object 2
+                r = x1 - x2
+                mag_r = np.abs(r)
+                norm_r = r / mag_r
+                mag_F = self.G / (mag_r**2) # can later multiply in masses
+                # force felt by 1 points at 2
+                F = - mag_F * norm_r
+                self.body_a[i] += F * self.body_m[j]
+                self.body_a[j] -= F * self.body_m[i]
 
-    def __init__(self,
-                 x = (0,0),
-                 v = (0,0),
-                 m = 1,
-                ) -> None:
-        self.x = x
-        self.v = v
-        self.a = (0,0)
-        self.m = m
-
-    def update_position():
-        pass
+        print(self.body_a)
+    
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import scipy as sp
 from matplotlib.animation import FuncAnimation
+import random
 
 matplotlib.use('TkAgg')
 
@@ -11,29 +12,32 @@ from classes import Universe
 if __name__ == '__main__':
     num_bodies = 2
     size = 2
-    G = 1
+    G = 0.1
+    dt = 0.01
 
-    universe = Universe(num_bodies, size, G)
+    universe = Universe(num_bodies, size, dt, G)
 
-    universe.body_x[0] = np.array([-1,0])
-    universe.body_x[1] = np.array([1, 0])
-    universe.body_v[0] = np.array([1,0])
-    universe.body_v[1] = np.array([-1,0])
+    for i in range(num_bodies):
+        x = random.uniform(-size/2, size/2)
+        y = random.uniform(-size/2, size/2)
+        universe.body_x[i] = np.array([x,y])
+        theta = np.arctan2(y,x)
+        universe.body_v[i] = 1e-2 * random.uniform(0.3,1) * np.array([-np.sin(theta), np.cos(theta)])
 
     fig, ax = plt.subplots()
     ax.set_xlim(-size, size)
     ax.set_ylim(-size, size)
     ax.set_aspect('equal')
 
-    points = ax.plot(universe.body_x[:,0], universe.body_x[:,1], 'o')
+    points, = ax.plot(universe.body_x[:,0], universe.body_x[:,1], 'o')
 
     def animate(i):
         universe.update_positions()
 
         points.set_data(universe.body_x[:,0], universe.body_x[:,1])
 
-        return points
+        return [points]
 
-    anim = FuncAnimation(fig, animate, frames=100, blit=True)
+    anim = FuncAnimation(fig, animate, frames=1000, interval=5*universe.dt, blit=True)
 
     plt.show()
